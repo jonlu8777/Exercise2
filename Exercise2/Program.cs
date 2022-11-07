79fe03a78a0dfc0e546df4015f5e7fea88b6d10c
Jonas Lundberg <jonlu8777@gmail.com>
2022-11-07 15:28:23 +0100

    100 >= age >=5

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -18,12 +18,12 @@
             Console.WriteLine("----- MENU:-----");
             Console.WriteLine("-Use integers to navigate-");
             Console.WriteLine("0. Exit");
-            Console.WriteLine("1. Youth / Retired / Standard");
-            Console.WriteLine("2. Calculate total ticket price for a group of people"); 
+            Console.WriteLine("1. Youth / Retired / Standard / Free of charge");
+            Console.WriteLine("2. Calculate total ticket price for a group of people (Max 10 people)"); 
             Console.WriteLine("3. Enter TEXT, print 10 times");
             Console.WriteLine("4. Enter a sentence, print the third word");
 
-                int navigateInt = ConsoleReadIntParse(); //Method to TryParse integers  
+                uint navigateInt = ConsoleReaduIntParse(); //Method to TryParse uIntegers  
 
              switch(navigateInt)
                 {
@@ -39,13 +39,16 @@
                         }
                     case 2:
                         {
-                            Console.WriteLine("How many tickets do you want:");  
-                           int numberOfTickets = ConsoleReadIntParse();      //Enter number of tickets
+                            Console.WriteLine("How many tickets do you want: (Max 10)");
+                            uint numberOfTickets = 11;
+                            while (numberOfTickets > 10)                         //Max 10 people in a group
+                            {
+                                numberOfTickets = ConsoleReaduIntParse();      //Enter number of tickets
+                            }
                             int sum = 0;
                             for (int i = 0; i < numberOfTickets; i++)
                             {
                                 sum += EnterAgeReturnPrice(i);               //calculate ticket price sum of a group             
-                              
                             }  
                                 Console.WriteLine($" Number of tickets: {numberOfTickets}, total price: {sum}"); // Print sum and number of persons
                             break;
@@ -97,12 +100,12 @@
 
         }
 
-        private static int ConsoleReadIntParse()
+        private static uint ConsoleReaduIntParse()
         {
             
             while (true)
             {
-                if (int.TryParse(Console.ReadLine()!, out int result))
+                if (uint.TryParse(Console.ReadLine()!, out uint result))
                 {
                     return result;   
                 }
@@ -110,38 +113,46 @@
             }
         } // ReadLine and IntTryParse, return int 
 
-        private static int EnterAgeReturnPrice(int i)
+        private static int EnterAgeReturnPrice(int i) //input value "i" is a counter to number of people in group
         {
             
             Console.WriteLine($"{i+1}.Enter age:");
-            int personAge = ConsoleReadIntParse(); 
+            uint personAge = ConsoleReaduIntParse(); 
 
-            if (personAge < 20) //check if youth
+            if (personAge <= 5 || personAge >= 100) // check if free of charge 
+            {
+                return 0;
+            }
+            else if (personAge < 20) //check if youth 
             {
                 
                 return 80;
             }
-            else if (personAge > 64) //check if retired
+            else if (personAge > 64) //check if retired 
             {
                
                 return 90;
             }
-            if (personAge >= 20 && personAge <= 64) //check if standard 
+            else if (personAge >= 20 && personAge <= 64) //check if standard 
             {
                 
                 return 120;
             }
-            return 0;   
-        } // Input: int age, return: int price depending on Youth/Retired/Standard
+            return 1;
+        } // Asks for age, return: int price depending on Youth/Retired/Standard
 
         private static void EnterAgePrintPrice()
         {
             
-            Console.WriteLine("Youth / Retired / Standard");
+            Console.WriteLine("Youth / Retired / Standard / Free of charge");
             Console.WriteLine("Enter age:");
-            int personAge = ConsoleReadIntParse(); 
+            uint personAge = ConsoleReaduIntParse(); 
 
-            if (personAge < 20) //Print if youth
+            if(personAge >= 100 || personAge <= 5) //Print "0kr" if very old or very young
+            {
+             Console.WriteLine("Free of charge: 0kr"); 
+            }
+           else if (personAge < 20) //Print if youth
             {
                 Console.WriteLine("Youth price: 80kr");
             }
@@ -149,7 +160,7 @@
             {
                 Console.WriteLine("Retired price: 90kr");
             }
-            if(personAge >= 20 && personAge <= 64) //Print if standrad 
+            else if(personAge >= 20 && personAge <= 64) //Print if standrad 
             {
             Console.WriteLine("Standard price 120kr");
             }
